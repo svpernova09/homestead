2e679a5efaf4d5bc7c961ef01eb50a79aad3f1ca
user <RadPaperDinosaur@proton.me>
2024-07-10 14:13:39 -0400

    Adding a new version now only requires adding the number to the array. File size greatly reduced and test statement replaced with one liner.

--- scripts/clear-variables.sh
+++ scripts/clear-variables.sh
@@ -6,6 +6,6 @@ versions=(5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2)
 
 sed -i '/# Set Homestead Environment Variable/,+1d' /home/vagrant/.profile
 
-for version in ${versions}; do
+for version in ${versions[@]}; do
 	[[ -f /etc/php/$version/fpm/pool.d/www.conf ]] && sed -i '/env\[.*/,+1d' /etc/php/$version/fpm/pool.d/www.conf
 done
